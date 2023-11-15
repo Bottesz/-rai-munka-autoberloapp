73e54462d9a4ae7d7c39ac758beb67d85dd5ebc2
Bottesz <botyaring@gmail.com>
2023-11-15 15:38:50 +0100

    autoberlo

---
+++ src/polgarbotond_autoberlo/PolgarBotond_autoberlo.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package polgarbotond_autoberlo;
+
+/**
+ *
+ * @author PolgárBotond(SZOFT_2
+ */
+public class PolgarBotond_autoberlo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
